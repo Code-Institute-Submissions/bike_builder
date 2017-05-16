0c706dc6736bccc4e406b1e33ddc182bd3c0ddb0
RandyMarsh288 <jonhill356@gmail.com>
2017-05-16 20:28:24 +0100

    gallery displays images which can also be uploaded by users via form

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
+from accounts.models import User
+
 
 # Create your models here.
+class Gallery(models.Model):
+
+    image = models.ImageField(upload_to='gallery')
+    description = models.CharField(max_length=100)
+    uploader = models.ForeignKey(User)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.description
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Gallery
+from .forms import GalleryForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def gallery(request):
+    images = Gallery.objects.all().order_by('-uploaded_at')
+    return render(request, 'gallery/gallery.html', {'images': images})
+
+
+@login_required(login_url='/login/')
+def upload_image(request):
+    if request.method == "POST":
+        form = GalleryForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            new_image = form.save(commit=False)
+            new_image.uploader = request.user
+            new_image.save()
+            return redirect('gallery')
+
+        else:
+            messages.error(request, "We were unable to submit the form. Please check your details and try again.")
+
+    else:
+        form = GalleryForm()
+
+    return render(request, 'gallery/gallery_form.html', {'form': form})
