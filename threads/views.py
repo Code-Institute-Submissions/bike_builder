4dacef532508fb2d653968446f76629168be44be
RandyMarsh288 <jonhill356@gmail.com>
2017-06-08 13:50:27 +0100

    changed new_post view button_text from Update Post to Save Post

--- threads/views.py
+++ threads/views.py
@@ -130,7 +130,7 @@ def new_post(request, thread_id):
     args = {
         'form': form,
         'form_action': reverse('new_post', args={thread.id}),
-        'button_text': 'Update Post'
+        'button_text': 'Save Post'
     }
 
     args.update(csrf(request))
