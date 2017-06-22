a51ca2a36d992ddd974dacdd43c9a83249d30f3c
RandyMarsh288 <jonhill356@gmail.com>
2017-06-22 11:39:25 +0100

    new simple tags for showing info for each subject re thread with the latest post. The tag for the link to the thread has a bug

--- threads/templatetags/thread_extras.py
+++ threads/templatetags/thread_extras.py
@@ -32,6 +32,18 @@ def last_posted_timing(thread):
     return arrow.get(_posts).humanize()
 
 
+# @register.filter
+# def threads_ordered_by_latest_post(subject):
+#     latest_posts = []
+#     for thread in subject.threads.all():
+#         thread_posts = thread.posts.all().order_by('created_at')
+#         latest_posts.append(latest_posts[latest_posts.count() - 1])
+#     subject_list = []
+#     for post in latest_posts:
+#         subject_list
+#     return
+
+
 # @register.simple_tag
 # def forum_display_name(username):
 #     return username.split("@")[0]
@@ -44,13 +56,79 @@ def last_posted_timing(thread):
 
 
 @register.simple_tag
+def subject_thread_with_last_post(subject):
+
+    def latest_thread_date(t):
+        return t.posts.all().order_by('-created_at')[0].created_at
+
+    thread_list = list(subject.threads.all())
+    thread_list.sort(reverse=True, key=latest_thread_date)
+    full_thread_name = thread_list[0].name[:60]
+    if len(full_thread_name) >= 60:
+        return full_thread_name[:60] + '...'
+    else:
+        return full_thread_name
+
+
+@register.simple_tag
+def latest_subject_post_comment(subject):
+
+    def latest_thread_date(t):
+        return t.posts.all().order_by('-created_at')[0].created_at
+
+    thread_list = list(subject.threads.all())
+    thread_list.sort(reverse=True, key=latest_thread_date)
+    full_post_comment = thread_list[0].posts.all().order_by('-created_at')[0].comment
+    abbreviated_post_comment = full_post_comment[3:(len(full_post_comment)-4)]
+    if len(abbreviated_post_comment) >= 150:
+        return abbreviated_post_comment[:150] + '...'
+    else:
+        return abbreviated_post_comment
+
+
+@register.simple_tag
+def latest_subject_post_date(subject):
+
+    def latest_thread_date(t):
+        return t.posts.all().order_by('-created_at')[0].created_at
+
+    thread_list = list(subject.threads.all())
+    thread_list.sort(reverse=True, key=latest_thread_date)
+    latest_post_date = thread_list[0].posts.all().order_by('-created_at')[0].created_at
+    return arrow.get(latest_post_date).humanize()
+
+
+@register.simple_tag
+def latest_post_thread_id(subject):
+
+    def latest_thread_date(t):
+        return t.posts.all().order_by('-created_at')[0].created_at
+
+    thread_list = list(subject.threads.all())
+    thread_list.sort(reverse=True, key=latest_thread_date)
+    print thread_list[0].id
+    return thread_list[0].id
+
+
+# @register.simple_tag
+# def last_subject_post(subject):
+#     latest_posts = []
+#     for thread in subject.threads.all():
+#         post_list = thread.posts.all().order_by('created_at')
+#         latest_posts.append(post_list[post_list.count()-1])
+#     last_posts = latest_posts.order_by('created_at')
+#     last_post = last_posts[last_posts.count()-1]
+#     return last_post
+
+
+@register.simple_tag
 def user_vote_button(thread, subject, user):
     vote = thread.poll.votes.filter(user_id=user.id).first()
 
     if not vote:
         if user.is_authenticated():
             link = """
-                <div class="col-md-3 btn-vote">
+                <div class="col-xs-3 btn-vote">
                     <a href="%s" class="btn btn-default btn-sm">
                         Add my vote!
                     </a>
