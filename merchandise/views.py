f37c070f1ac075afde817c8619789462d0e1bf40
RandyMarsh288 <jonhill356@gmail.com>
2017-06-28 14:31:06 +0100

    objects now ordered on price

--- merchandise/views.py
+++ merchandise/views.py
@@ -5,5 +5,5 @@ from .models import Merchandise
 # Create your views here.
 
 def all_products(request):
-    products = Merchandise.objects.all()
+    products = Merchandise.objects.all().order_by('price')
     return render(request, 'merchandise/merchandise.html', {"products": products})
