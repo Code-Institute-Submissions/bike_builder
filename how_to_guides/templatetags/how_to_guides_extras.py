eb1949583791a9be1b6becc1a315df9f0ad5f8d6
RandyMarsh288 <jonhill356@gmail.com>
2017-07-11 14:21:42 +0100

    contents deleted as any testing of poll functionality will be in the threads testing

--- how_to_guides/templatetags/how_to_guides_extras.py
+++
@@ -1,9 +0,0 @@
-# from django import template
-#
-# register = template.Library()
-#
-#
-# @register.simple_tag
-# def poster_display_name(guide):
-#     i = guide.uploader.username
-#     return i.split("@")[0]
--- polls/tests.py
+++ polls/tests.py
@@ -1,12 +1,4 @@
-from django.test import TestCase
-from views import place_order
-from django.core.urlresolvers import resolve
-from django.shortcuts import render_to_response
+from __future__ import unicode_literals
+from django.db import models
 
-
-# URL test
-# Place_order also has an arg (product_id): does this need to be included in the url?
-class PaymentsPageTest(TestCase):
-    def test_payments_page_resolves(self):
-        get_place_order_page = resolve('/place_order/')
-        self.assertEqual(get_place_order_page.func, place_order)
+# Create your models here.
