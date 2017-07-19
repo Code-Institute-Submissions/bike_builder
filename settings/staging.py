21e0f2812fb991c4b894a2c3cb9436593e516088
RandyMarsh288 <jonhill356@gmail.com>
2017-07-19 10:31:08 +0100

    changes for deployment

--- settings/staging.py
+++ settings/staging.py
@@ -1,4 +1,5 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -8,6 +9,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_kLdDMq39gQJuwVRHNbtROldY')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_sNgcHOXBxiaZuVTDDAnX2AIQ')
