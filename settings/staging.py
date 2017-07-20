df01c1235987716e3e3e7f018198d9657a70bc65
RandyMarsh288 <jonhill356@gmail.com>
2017-07-20 11:59:42 +0100

    deployment

--- settings/staging.py
+++ settings/staging.py
@@ -3,14 +3,16 @@ import dj_database_url
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
+# DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
 
-DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
+DATABASES = dj_database_url.config("CLEARDB_DATABASE_URL")
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_kLdDMq39gQJuwVRHNbtROldY')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_sNgcHOXBxiaZuVTDDAnX2AIQ')
