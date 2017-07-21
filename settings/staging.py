2a2709e358e864165fb08e97d7c4a8735a11df4c
RandyMarsh288 <jonhill356@gmail.com>
2017-07-21 14:27:17 +0100

    deployment

--- settings/staging.py
+++ settings/staging.py
@@ -3,16 +3,9 @@ import dj_database_url
 
 DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-#
-# DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
-
-DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('CLEARDB_DATABASE_URL'))
+}
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_kLdDMq39gQJuwVRHNbtROldY')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_sNgcHOXBxiaZuVTDDAnX2AIQ')
