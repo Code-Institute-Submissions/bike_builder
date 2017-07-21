1164e3f5d3bbe15b772ad80dabde0663c6b0b9b6
RandyMarsh288 <jonhill356@gmail.com>
2017-07-21 10:22:24 +0100

    deployment

--- settings/staging.py
+++ settings/staging.py
@@ -12,7 +12,7 @@ DEBUG = False
 #
 # DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
 
-DATABASES = dj_database_url.config("CLEARDB_DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_kLdDMq39gQJuwVRHNbtROldY')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_sNgcHOXBxiaZuVTDDAnX2AIQ')
