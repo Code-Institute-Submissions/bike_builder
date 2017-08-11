7386e3346837a55ae8231c6af4ef0c44a2c0ec91
RandyMarsh288 <jonhill356@gmail.com>
2017-08-11 16:05:44 +0100

    Drop down list of manufacturers now appears in alphabetical order. A set is no longer used to remove duplicates as it is unordered in nature. Replacement code not as concise but manual testing shows it to have worked. Also improved code by replacing a,b,c and x with more descriptive names.

--- bike_selector/forms.py
+++ bike_selector/forms.py
@@ -11,10 +11,13 @@ def manufacturer_list():
     Create a list of tuples of unique manufacturers 
     for the drop-down menu
     """
-    a = set(bike.manufacturer for bike in bike_list)
-    b = ['All'] + (list(a))
-    c = sorted([(x, x) for x in b])
-    return c
+    man_list = []
+    for bike in bike_list:
+        if bike.manufacturer not in man_list:
+            man_list.append(bike.manufacturer)
+    man_tuples = [(manufacturer, manufacturer) for manufacturer in man_list]  # create a list of tuples
+    complete_man_tuples = [('All', 'All')] + man_tuples  # add 'All' here so it appears at top of list
+    return complete_man_tuples
 
 
 def cylinders_list():
@@ -22,10 +25,11 @@ def cylinders_list():
     Create a list of tuples of unique number of 
     cylinders for the drop-down menu
     """
-    a = set(bike.cylinders for bike in bike_list)
-    b = ['All'] + list(a)
-    c = [(x, x) for x in b]
-    return c
+    cyl_list = list(set(bike.cylinders for bike in bike_list))
+    cyl_tuples = [(cylinders, cylinders) for cylinders in cyl_list]
+    cyl_tuples.sort()
+    complete_cyl_tuples = [('All', 'All')] + cyl_tuples
+    return complete_cyl_tuples
 
 
 def layout_list():
@@ -33,10 +37,11 @@ def layout_list():
     Create a list of tuples of unique engine 
     layouts for the drop-down menu
     """
-    a = set(bike.layout for bike in bike_list)
-    b = ['All'] + list(a)
-    c = [(x, x) for x in b]
-    return c
+    lay_list = list(set(bike.layout for bike in bike_list))
+    lay_tuples = [(layout, layout) for layout in lay_list]
+    lay_tuples.sort()
+    complete_lay_tuples = [('All', 'All')] + lay_tuples
+    return complete_lay_tuples
 
 
 class BikeSearchForm(forms.Form):
@@ -49,5 +54,58 @@ class BikeSearchForm(forms.Form):
     layout = forms.ChoiceField(choices=layout_list, initial='All')
 
     class Meta:
-        fields = ("name", 'cylinders', 'layout')
+        fields = ('name', 'cylinders', 'layout')
 
+# from django import forms
+# from .models import Bike
+#
+#
+# #  set in global scope so can be used by all list functions
+# bike_list = Bike.objects.all().order_by('manufacturer')
+#
+#
+# def manufacturer_list():
+#     """
+#     Create a list of tuples of unique manufacturers
+#     for the drop-down menu
+#     """
+#     a = set(bike.manufacturer for bike in bike_list)
+#     b = ['All'] + (list(a))
+#     c = sorted([(x, x) for x in b])
+#     return c
+#
+#
+# def cylinders_list():
+#     """
+#     Create a list of tuples of unique number of
+#     cylinders for the drop-down menu
+#     """
+#     a = set(bike.cylinders for bike in bike_list)
+#     b = ['All'] + list(a)
+#     c = [(x, x) for x in b]
+#     return c
+#
+#
+# def layout_list():
+#     """
+#     Create a list of tuples of unique engine
+#     layouts for the drop-down menu
+#     """
+#     a = set(bike.layout for bike in bike_list)
+#     b = ['All'] + list(a)
+#     c = [(x, x) for x in b]
+#     return c
+#
+#
+# class BikeSearchForm(forms.Form):
+#     """
+#     Form with dropdown menus from which user
+#     selects criteria
+#     """
+#     manufacturer = forms.ChoiceField(choices=manufacturer_list, initial='All')
+#     cylinders = forms.ChoiceField(choices=cylinders_list, initial='All')
+#     layout = forms.ChoiceField(choices=layout_list, initial='All')
+#
+#     class Meta:
+#         fields = ("name", 'cylinders', 'layout')
+#
